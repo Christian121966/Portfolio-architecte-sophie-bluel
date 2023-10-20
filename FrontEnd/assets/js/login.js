b67fde9b8b3d1dcfd32706a8e2c28a85f9313bd7
Christian121966 <boissychristian@gmail.com>
2023-10-20 10:44:01 +0200

    modal

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,107 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
+window.onload = function() {
     const loginForm = document.querySelector('#login-form');
     const errorMessage = document.querySelector('#message-error');
     const buttonConnect = document.querySelector('.button-connect');
-
-    const loginUrl = "https://localhost:5678/api/users/login";
-
-    const login = async (email, password) => {
-        try {
-            const Response = await fetch(loginUrl, {
-                method : "POST" ,
-                headers: {
-                    "content-type": "application/json" ,
-                },
-                body: JSON.stringify({
-                    email: encodeURIComponent(email),
-                    password: encodeURIComponent(password),
-                }),
-            });
-            if (Response.status === 200) {
-                const ResponseJson = await Response.json();
-                const token = ResponseJson.token;
-                localStorage.setItem("token" , token);
-                window.location.href = "./index.html";
-            } else {
-                errorMessage.textContent = "Email ou mot de passe incorrect";
-                buttonConnect.classList.add("animationbtn");
-            }
-        } catch (error) {
-            logger.error(error);
-        }
-    }
-
     buttonConnect.addEventListener('click', async (e) => {
+        console.log("Début de l'écouteur de clic");
         e.preventDefault();
-        logger.debug('click');
-        const email = loginForm.email.value;
-        const password = loginForm.password.value;
-        if (validateEmail(email) && validatePassword(password)) {
-            try {
-                const response = await login(email, password);
-                if (response && response.token) {
-                    const token = response.token;
-                    localStorage.setItem("token", token);
-                    window.location.replace("./index.html");
-                } else {
-                    errorMessage.textContent = "Email ou mot de passe incorrect";
+        console.log('click'); // Utilisation de console au lieu de logger
+
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+          email: loginForm.username.value,
+          password: loginForm.password.value,
+        });
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("http://localhost:5678/api/users/login", requestOptions)
+            .then(response => {
+                if (response.status === 404 || response.status === 401) {
+                    errorMessage.textContent = "Invalid username or password";
                     buttonConnect.classList.add("animationbtn");
-                }
-            } catch(error) {
-                if (error instanceof NetworkError) {
-                    logger.error('Network error occurred');
-                } else if (error instanceof ApiError) {
-                    logger.error('API error occured');
                 } else {
-                    logger.error('An error occured');
+                    return response.json();
                 }
-            }
-        } else {
-            errorMessage.textContent = "Invalid email or password";
-            buttonConnect.classList.add("animationbtn");
-        }
+            })
+            .then(result => {
+
+                localStorage.setItem('token', result.token);
+                window.location.href = "./index.html";
+                //ok
+                //sauvgarder le token ==== result.token
+                //local storage ==>
+                //changer la page
+                // redirection en js
+                console.log(result)
+            })
+        .catch(error => console.log('error', error));
+
     });
-})
 
-
-buttonConnect.addEventListener('click', async (e) => {
-    e.preventDefault();
-    logger.debug('click');
-    const email = loginForm.email.value;
-    const password = loginForm.password.value;
-    if (validateEmail(email) && validatePassword(password)) {
-        try {
-            const response = await login(email, password);
-                if (response && response.token) {
-                    const token = response.token;
-                    localStorage.setItem("token", token);
-                    window.location.replace("./index.html");
-                    logger.info(token);
-                } else {
-                    errorMessage.textContent = "Email ou mot de passe incorrect";
-                    buttonConnect.classList.add("animationbtn");
-                }
-            } catch(error) {
-                if (error instanceof NetworkError) {
-                    logger.error('Network error occurred');
-                } else if (error instanceof ApiError) {
-                    logger.error('API error occured');
-                } else {
-                    logger.error('An error occured');
-                }
-            }
-            
-    } else {
-        errorMessage.textContent = "Invalid email or password";
-        buttonConnect.classList.add("animationbtn");
-    }
-});
-
-
-function validateEmail(email) {
-    return true; //true si l'email est valide
-}
-
-function validatePassword(password) {
-    return true; //true si le password est valide
+   
 }
---
+++ FrontEnd/assets/js/script.js
@@ -0,0 +1,67 @@
+
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const gallery = document.getElementById("gallery");
+let datas = [];
+
+// Chargez les données initiales ici
+fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(function (data) {
+        datas = data;
+        //recupere la liste des categories
+        let categories = [];
+        createFilter("Tous");
+        data.forEach(work => {
+            if (!categories.includes(work.category.name)) {
+                categories.push(work.category.name);
+                createFilter(work.category.name);
+            }
+        });
+        updateGallery('Tous'); // Mettez à jour la galerie avec 'Tous' par défaut
+    });
+
+
+
+
+
+// Associez un gestionnaire d'événements à chaque bouton de filtre
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const filterValue = button.getAttribute('data-filter');
+        updateGallery(filterValue); // Mettez à jour la galerie avec la catégorie du bouton cliqué
+    });
+});
+
+
+//cree les boutons
+function createFilter(categorie) {
+    let button = document.createElement("button");
+    button.setAttribute("data-filter", categorie);
+    button.classList.add("objects");
+    button.textContent = categorie;
+    document.querySelector("#filter-buttons").appendChild(button);
+    button.addEventListener("click", () => {
+      const filterValue = button.getAttribute("data-filter");
+      updateGallery(filterValue); // Mettez à jour la galerie avec la catégorie du bouton cliqué
+    });
+}
+function updateGallery(activeCategory) {
+gallery.innerHTML = "";
+
+datas.forEach((work) => {
+if (activeCategory === "Tous" || work.category.name === activeCategory) {
+  const figure = document.createElement("figure");
+  figure.setAttribute("data-category", work.category.name);
+  const img = document.createElement("img");
+  img.src = work.imageUrl;
+  img.alt = work.title;
+  img.setAttribute("data-category", work.category.name);
+  const figcaption = document.createElement("figcaption");
+  figcaption.textContent = work.title;
+
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
+});
+}
